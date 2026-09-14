48191d8b02402bea5833feb39daa7567ff5402b9
Johan-v-p <pradojohan805@gmail.com>
2026-09-14 18:06:11 -0500

    feat(docker): agregar Dockerfile y docker-compose

--- migrations/env.py
+++ migrations/env.py
@@ -6,6 +6,7 @@ from app.models.sesion_tecnologia import SesionTecnologia
 from app.models.sesion import Sesion
 from app.models.tecnologia import Tecnologia
 
+
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
@@ -32,24 +33,15 @@ target_metadata = Base.metadata
 # ... etc.
 
 
-def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
-    url = config.get_main_option("sqlalchemy.url")
-    context.configure(
+def run_migrations_online() -> None:
+    import os
+    url = os.environ.get("DATABASE_URL") or config.get_main_option("sqlalchemy.url")
+    
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
         url=url,
-        target_metadata=target_metadata,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
     )
 
     with context.begin_transaction():
